ada17628b2f868a87ad07188168f11980339b56f
SanzharNiyazbek <124326329+SanzharNiyazbek@users.noreply.github.com>
2023-02-24 12:20:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let Teams = [
+{
+  name: 'Dallas Mavericks',
+  favPlayer: 'Luka Doncic',
+  titles: 0,
+  
+},
+{
+  name: 'Los Angeles Lakers',
+  favPlayer: 'LeBron James',
+  titles: 4,
+}, 
+{
+  name: 'Chicago Bulls',
+  favPlayer: 'Michel Jordan',
+  titles: 6,
+}
+]
+
+
+for (let i = 0; i < Teams.length; i++) {
+  document.write("<h5>Team: " + Teams[i]["name"] + "</h5>");
+  document.write("<h5>Favourite player: " + Teams[i]["favPlayer"] + "</h5>");
+  document.write("<h5>Titles: " + Teams[i]["titles"] + "</h5>");
+  document.writeln("")
+}
+
+function re(Teams){
+  return re;
+}
+console.log(Teams)
+
+
+
+
